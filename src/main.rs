4ec56fa4e839132bd70552e6cc70426e9fa3b8ef
mendelsshop <mendelsshop@gmail.com>
2026-08-10 20:12:16 -0400

    started working on '<leader>[count]e' and '<leader>[count]ge' for moving boundries of commentary by a fixed amount of words

--- src/main.rs
+++ src/main.rs
@@ -610,61 +610,75 @@ fn run(mut terminal: DefaultTerminal, mut app: AppState<'_>) -> Result<()> {
                     },
                 ) => {
                     // TODO: [count]e
-                    if command_buffer == " " {
-                        command_buffer.clear();
-                        let line = &mut current.text[position.0];
-                        // make sure its not inside a word boundary
-                        let line_position = get_line_position(position, line, true);
-                        let line_position_new =
-                            get_line_position(&(position.0, position.1 + 1), line, true);
+                    if command_buffer.starts_with(' ') {
+                        let mut command = command_buffer.chars();
+                        _ = command.next();
+                        let _number = command
+                            .by_ref()
+                            .map_while(|n| n.to_digit(10))
+                            .reduce(|acc, n| acc * 10 + n)
+                            .unwrap_or(1);
+                        match command.next() {
+                            None => {
+                                command_buffer.clear();
+                                let line = &mut current.text[position.0];
+                                // make sure its not inside a word boundary
+                                let line_position = get_line_position(position, line, true);
+                                let line_position_new =
+                                    get_line_position(&(position.0, position.1 + 1), line, true);
 
-                        // If its last word and you try to move it foward it will wrap to end of
-                        // current line, maybe allow it to go foward a line if possible
-                        log::info!(" e: {line_position} {line_position_new}");
-                        if
-                        // make sure we are not passing any other commentaries (behaviour for now)
-                        line
-                            .commentary
-                            .keys()
-                            .skip_while(|n| **n <= line_position)
-                            .next()
-                            .is_none_or(|n| *n < line_position_new)
-                            && let Some(commentary) = line.commentary.remove(&line_position)
-                        {
-                            line.commentary.insert(line_position_new, commentary);
-                            if position.1 < line.len {
-                                position.1 = line_position_new;
+                                // If its last word and you try to move it foward it will wrap to end of
+                                // current line, maybe allow it to go foward a line if possible
+                                log::info!(" e: {line_position} {line_position_new}");
+                                if
+                                // make sure we are not passing any other commentaries (behaviour for now)
+                                line
+                                    .commentary
+                                    .keys()
+                                    .find(|n| **n > line_position)
+                                    // .skip_while(|n| **n <= line_position)
+                                    // .next()
+                                    .is_none_or(|n| *n > line_position_new)
+                                    && let Some(commentary) = line.commentary.remove(&line_position)
+                                {
+                                    line.commentary.insert(line_position_new, commentary);
+                                    if position.1 < line.len {
+                                        position.1 = line_position_new;
+                                    }
+                                }
                             }
-                        }
-                    }
-                    if command_buffer == " g" {
-                        command_buffer.clear();
-                        let line = &mut current.text[position.0];
-                        // make sure its not inside a word boundary
-                        let line_position = get_line_position(position, line, true);
-                        // If its first word and you try to move it back it won't work, maybe allow
-                        // it to go back a line if possible
-                        if let Some(line_position_new) = line
-                            .words
-                            .iter()
-                            .take_while(|x| line_position > x.0.end)
-                            .last()
-                            .map(|(std::ops::Range { end, .. }, _)| *end)
-                        {
-                            log::info!(" ge: {line_position} {line_position_new}");
-                            if
-                            // make sure we are not passing any other commentaries (behaviour for now)
-                            line
-                                .commentary
-                                .keys()
-                                .take_while(|n| **n < line_position)
-                                .last()
-                                .is_none_or(|n| *n < line_position_new)
-                                && let Some(commentary) = line.commentary.remove(&line_position)
-                            {
-                                line.commentary.insert(line_position_new, commentary);
-                                position.1 = line_position_new;
+                            Some('g') if command.next().is_none() => {
+                                command_buffer.clear();
+                                let line = &mut current.text[position.0];
+                                // make sure its not inside a word boundary
+                                let line_position = get_line_position(position, line, true);
+                                // If its first word and you try to move it back it won't work, maybe allow
+                                // it to go back a line if possible
+                                if let Some(line_position_new) = line
+                                    .words
+                                    .iter()
+                                    .take_while_inclusive(|x| line_position > x.0.end)
+                                    .last()
+                                    .map(|(std::ops::Range { end, .. }, _)| *end)
+                                {
+                                    log::info!(" ge: {line_position} {line_position_new}");
+                                    if
+                                    // make sure we are not passing any other commentaries (behaviour for now)
+                                    line
+                                        .commentary
+                                        .keys()
+                                        .take_while(|n| **n < line_position)
+                                        .last()
+                                        .is_none_or(|n| *n < line_position_new)
+                                        && let Some(commentary) =
+                                            line.commentary.remove(&line_position)
+                                    {
+                                        line.commentary.insert(line_position_new, commentary);
+                                        position.1 = line_position_new;
+                                    }
+                                }
                             }
+                            _ => {}
                         }
                     }
                 }
